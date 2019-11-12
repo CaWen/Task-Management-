e153a5e38578df3a2c4594a1527127b90072a49f
shizhaochen1997 <36032038+shizhaochen1997@users.noreply.github.com>
2019-11-11 23:19:49 -0600

    LoginPage_v1
    login page v1

---
+++ login.js
@@ -0,0 +1,21 @@
+function login() {
+    var username = document.getElementById("username");
+    var pass = document.getElementById("password");
+
+    if (username.value == "") {
+ 
+        alert("Please enter username");
+ 
+    } else if (pass.value  == "") {
+ 
+        alert("Please enter password");
+ 
+    } else if(username.value == "admin" && pass.value == "123456"){
+ 
+        window.location.href="welcome.html";
+ 
+    } else {
+ 
+        alert("Please enter correct username and password")
+    }
+}
