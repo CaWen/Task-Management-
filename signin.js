4fe4a03ffbbee470a05604a68bceaca6d28b7b3d
shizhaochen1997 <36032038+shizhaochen1997@users.noreply.github.com>
2019-11-12 20:26:29 -0600

    LoginPage_Ver2

--- signin.js
+++ signin.js
@@ -1,21 +1,23 @@
 function login() {
-    var username = document.getElementById("username");
-    var pass = document.getElementById("password");
 
+    var username = document.getElementById("inputUsername");
+    var pass = document.getElementById("inputPassword");
+  
     if (username.value == "") {
- 
+    
         alert("Please enter username");
- 
+    
     } else if (pass.value  == "") {
- 
+        
         alert("Please enter password");
- 
+    
     } else if(username.value == "admin" && pass.value == "123456"){
- 
-        window.location.href="welcome.html";
- 
+        
+        window.location.href="homepage.html";
+   
     } else {
- 
+   
         alert("Please enter correct username and password")
+    
     }
 }
